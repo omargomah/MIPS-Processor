e5d24144184006e8615659a388ace2633a643e47
Omar Gomaa <mrjmh934@gmail.com>
2025-11-12 20:41:58 +0200

    add the component Mux and test it

--- Mux.vhd
+++ Mux.vhd
@@ -1,14 +1,14 @@
+
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-entity Adder is
-    port (  
-        A: in  std_logic_vector(31 downto 0);
-        B: in  std_logic_vector(31 downto 0);
-        Sum     : out std_logic_vector(31 downto 0);
-    );
-end entity Addert;
-architecture Behavioral of Adder is
+entity Mux is
+    port (
+       A: in  std_logic_vector(31 downto 0);
+       B: in  std_logic_vector(31 downto 0);
+       Sel: in  std_logic;
+       Y: out std_logic_vector(31 downto 0));
+end  Mux ;
+architecture behavier of Mux  is
 begin
-    Sum <= std_logic_vector(unsigned(A) + unsigned(B));
-end architecture Behavioral;
+    Y<= B when Sel = '1' else A;
+end behavier;
